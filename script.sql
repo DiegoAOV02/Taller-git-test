dd7dd2928fe8158c995b95f47e28757bb1a6672f
Diego Ortiz <2030282@upv.edu.mx>
2024-10-18 13:08:11 -0600

    Cambios a bd

---
+++ script.sql
@@ -0,0 +1,12 @@
+drop table Alumnos:
+drop table profesores;
+
+create table alumnos (
+  id int not null primary key,
+  name varchar(20) not null,
+);
+
+create table profesores (
+  id int not null primary key,
+  name varchar(20) not null
+);
